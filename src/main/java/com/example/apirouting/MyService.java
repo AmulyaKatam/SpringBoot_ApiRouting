46cfb16902bf07e5d67237b04298ac313f3f2f68
Katam Amulya <amulya.katam777@gmail.com>
2023-03-02 17:23:41 +0000

    solution

---
+++ src/main/java/com/example/apirouting/MyService.java
@@ -0,0 +1,14 @@
+// Write your code here. 
+package com.example.apirouting;
+
+public class MyService{
+    public String getHome(){
+        return "Home Page";
+    }
+
+    public String getAbout(){
+        return "About Page";
+    }
+}
+
+     
